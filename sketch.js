cbc4780c5a001dfe0ad2af25d0d411b87a97976b
tishyaDesigns <72818577+tishyaDesigns@users.noreply.github.com>
2021-08-14 08:44:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var ball = {
+  x:20,
+  y:30,
+  r:30,
+  xspeed:0,
+  yspeed:0, 
+  color:["blue","red","green","purple"],
+ 
+};
+
+function setup() 
+{
+  createCanvas(400, 400);
+ 
+}
+
+function draw() 
+{
+  background(220);
+      
+}
+
